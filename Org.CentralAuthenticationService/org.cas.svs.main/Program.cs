9097261168a15ceef16e7b655663078aad0155eb
alfeosilvestro <alfeo.silvestro@live.com>
2015-02-25 22:08:27 +0800

    owin integration initial checkin

--- Org.CentralAuthenticationService/org.cas.svs.main/Program.cs
+++ Org.CentralAuthenticationService/org.cas.svs.main/Program.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Configuration;
+using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace org.cas.svs.main
 {
@@ -10,7 +10,26 @@ namespace org.cas.svs.main
     {
         static void Main(string[] args)
         {
-            Console.Read();
+            string baseAddress = ConfigurationManager.AppSettings["BaseAddress"];
+
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                // Create HttpCient and make a request to api/values 
+                //HttpClient client = new HttpClient();
+
+                //var response = client.GetAsync(baseAddress + "api/values").Result;
+
+                //Console.WriteLine(response);
+                //Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+
+                Console.WriteLine(ConfigurationManager.AppSettings["WelcomeMessage"]);
+                Console.WriteLine("Press [Enter] key to bring the service offline.");
+                Console.ReadLine(); 
+            }
+
+            Console.WriteLine("Are you sure you want to bring the service offline? Press [Enter] to continue.");
+            Console.ReadLine(); 
         }
     }
 }
